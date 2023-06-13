225a6c69ffb194c2b5f31d36fd68f676e5e3515f
walfe310831 <245152695@qq.com>
2023-06-13 15:56:04 +0800

    1

--- src/nc/mairo/Mushroom.java
+++ src/nc/mairo/Mushroom.java
@@ -1,14 +1,7 @@
 package nc.mairo;
 
 public class Mushroom {
-  public int getMushroom() {
-		return mushroom;
-	}
 
-	public void setMushroom(int mushroom) {
-		this.mushroom = mushroom;
-	}
-
-public int mushroom;
+public int mushroomB;
   
 }
